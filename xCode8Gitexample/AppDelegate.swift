a9468e77a4c315b8819b6691c0d3d27a1d6781ae
Jay Llanos <jimmy@Jimmys-MacBook-Pro.local>
2016-11-25 15:21:37 -0500

    Initial Commit

---
+++ xCode8Gitexample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  xCode8Gitexample
+//
+//  Created by Jay Llanos on 11/25/16.
+//  Copyright © 2016 Jay Llanos. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ xCode8Gitexample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  xCode8Gitexample
+//
+//  Created by Jay Llanos on 11/25/16.
+//  Copyright © 2016 Jay Llanos. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
